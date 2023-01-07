1d239c3c45b54b7abebcf1fe51ecb6adc0a4b1e7
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2023-01-07 23:28:11 +0530

    Button, ButtonGroup, IconButton, and ToggleButton

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,12 +1,69 @@
-import React from 'react'
-import {Stack, Button} from '@mui/material'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React,{useState} from 'react'
+import {Stack, Button, IconButton, ButtonGroup, ToggleButton, ToggleButtonGroup} from '@mui/material'
+import {Send, FormatBold, FormatItalic, FormatUnderlined} from '@mui/icons-material'
 
 const MuiButton = () => {
+    const [format, setFormat] = useState<string| null>(null);
+
+    const handleToggleChange =(event: React.MouseEvent<HTMLElement>, updatedFormat:string|null)=>{
+        setFormat(updatedFormat)
+        console.log(format);
+    }
+    
   return (
+    <Stack spacing={4}>
     <Stack spacing={2} direction='row'>
-      <Button variant='text'>Text</Button>
+      <Button variant='text' href='https://mui.com/material-ui/customization/default-theme/' >Text</Button>
       <Button variant='contained'>contained</Button>
       <Button variant='outlined'>outlined</Button>
+    </Stack>
+    <Stack spacing={2} direction='row'>
+        <Button variant='contained'color='primary'>Primary</Button>
+        <Button variant='contained'color='secondary'>secondary</Button>
+        <Button variant='contained' color='error'>Error</Button>
+        <Button variant='contained' color='warning' >Warning</Button>
+        <Button variant='contained' color='success'>Success</Button>
+    </Stack>
+    {/* <Stack spacing={2} direction='row'>
+        <Button variant='outlined'color='primary'>Primary</Button>
+        <Button variant='outlined'color='secondary'>secondary</Button>
+        <Button variant='outlined' color='error'>Error</Button>
+        <Button variant='outlined' color='warning' >Warning</Button>
+        <Button variant='outlined' color='success'>Success</Button>
+    </Stack> */}
+    {/* <Stack display='block' spacing={2} direction='row'>
+        <Button variant='text'color='secondary'>secondary</Button>
+        <Button variant='text' color='error'>Error</Button>
+        <Button variant='text' color='warning' >Warning</Button>
+        <Button variant='text' color='success'>Success</Button>
+    </Stack> */}
+    <Stack display='block' spacing={2} direction='row'>
+        <Button variant='contained'size='small' color='secondary'>secondary</Button>
+        <Button variant='contained'size='medium'  color='error'>Error</Button>
+        <Button variant='contained'size='large'  color='success'>Success</Button>
+    </Stack>
+    {/* <Stack display='block' spacing={2} direction='row'>
+        <Button variant='contained'color='secondary' disableRipple onClick={()=> alert('clicked')} startIcon={<Send/>}>Send</Button>
+        <Button variant='contained'color='warning' disableElevation endIcon={<Send/>}>Send</Button>
+        <IconButton color='success' size='small'>
+            <Send/>
+        </IconButton>
+    </Stack> */}
+    {/* <Stack direction='row'> 
+        <ButtonGroup variant='contained' color='warning' size='medium' orientation='vertical' aria-label='alignment group'>
+            <Button>Left</Button>
+            <Button>Center</Button>
+            <Button>Right</Button>
+        </ButtonGroup>
+    </Stack> */}
+    <Stack direction='row'> 
+        <ToggleButtonGroup size='large' color='warning'exclusive aria-label='alignment group' value={format} onChange={handleToggleChange} >
+            <ToggleButton value={'bold'}><FormatBold /></ToggleButton>
+            <ToggleButton value={'italic'}><FormatItalic /></ToggleButton>
+            <ToggleButton value={'underlined'}><FormatUnderlined /></ToggleButton>
+        </ToggleButtonGroup>
+    </Stack>
     </Stack>
   )
 }
--- src/components/MuiTypography.tsx
+++ src/components/MuiTypography.tsx
@@ -6,11 +6,11 @@ export const MuiTypography = () => {
   <div>
     <Typography variant="h1">h1 heading</Typography>
     <Typography variant="h2">h2 heading</Typography>
-    <Typography variant="h3">h3 heading</Typography>
-    <Typography variant="h4">h4 heading</Typography>
+    {/* <Typography variant="h3">h3 heading</Typography>
+    <Typography variant="h4">h4 heading</Typography> */}
     {/* <Typography variant="h4" component='h1' gutterBottom>h4 style but h1 components and margin bottom = gutterBottom heading</Typography> */}
-    <Typography variant="h5">h5 heading</Typography>
-    <Typography variant="h6">h6 heading</Typography>
+    {/* <Typography variant="h5">h5 heading</Typography>
+    <Typography variant="h6">h6 heading</Typography> */}
     {/* <Typography variant="subtitle1">subtitle 1</Typography>
     <Typography variant="subtitle2">subtitle 2</Typography>
     <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente omnis modi similique ad tenetur recusandae sit ipsam, officiis neque nesciunt vero earum sint reprehenderit id fugiat est quod et? Repudiandae?</Typography>
